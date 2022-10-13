4c0c94ae16adc15f34d6f54064c62a35e53d690a
EmilyALaBelle <emilylabelle8@gmail.com>
2022-10-13 09:35:30 -0400

    second

---
+++ guess.js
@@ -0,0 +1,7 @@
+/*
+create a program that guesses a number from one to ten then asks us to guess that number. 
+If it is higher say "to high", and ask again.
+ If it is lower say "to low" and ask again.
+If it is the right number say "congratulations" and exit
+*/
+import readline from "readline-sync"
