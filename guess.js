2e4536ccea70c121b871a8d2028f24deea6124c9
EmilyALaBelle <emilylabelle8@gmail.com>
2022-10-13 10:38:03 -0400

    third

--- guess.js
+++ guess.js
@@ -5,3 +5,22 @@ If it is higher say "to high", and ask again.
 If it is the right number say "congratulations" and exit
 */
 import readline from "readline-sync"
+
+let userGuessRight = false
+
+//while(true){ me playing
+//guess a number from 1-10
+let numberChosen = Math.ceil(Math.random() * 10)
+//console.log(numberChosen)
+//}
+while (!userGuessRight) {
+    let guess = readline.question("What is your guess?")
+    if (guess > numberChosen) {
+        console.log('Your guess is too high')
+    } else if (guess < numberChosen) {
+        console.log('Your guess is too low')
+    } else {
+        console.log('congratulations!')
+        userGuessRight = true
+    }
+}
